d96f6bd25d460841ae3eac0d49165c1589cb86f1
Davian D <d33.durrant@gmail.com>
2019-10-25 09:17:11 -0500

    adding loop var

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-'use strict'
+'use strict' 
+function fullName(){
 
+    var firstName = prompt('what is your first name?')
+    var lastName = prompt('what is your last name?')
 
-var favoriteinstrument = prompt('which instrument do you like?'); 
+    var full = (firstName + ' ' + lastName)
+    
+    alert ("Pleasure to have you! " + full);
+
+    return full;
+}
+
+//function favoriteInstrument(){
+
+    var favoriteInstrument = prompt('which instrument do you like?') 
+
+    if (favoriteInstrument === 'guitar')
+{
+    alert ('Congratulations + 'full', the next Bob Marley ');
+}
+    else
+    {
+    alert('Not a bad choice!,Guitars are still better :P');
+    }
+
+}
