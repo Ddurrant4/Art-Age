1e650b4f1408f5fe2ca9016f89ac202d4cabe54b
Davian D <d33.durrant@gmail.com>
2019-10-25 14:38:45 -0500

    adding while funcions

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 'use strict' 
+
 function fullName(){
-
-    var firstName = prompt('what is your first name?')
-    var lastName = prompt('what is your last name?')
-
-    var full = (firstName + ' ' + lastName)
+    var firstName = prompt('What is your first name?');
+    var lastName = prompt('What is your last name?');
     
-    alert ("Pleasure to have you! " + full);
-
+    var full = (firstName + ' ' + lastName);
+    alert ('Pleasure to have you! ' + full);
     return full;
-}
-
-//function favoriteInstrument(){
-
-    var favoriteInstrument = prompt('which instrument do you like?') 
-
-    if (favoriteInstrument === 'guitar')
-{
-    alert ('Congratulations + 'full', the next Bob Marley ');
-}
-    else
-    {
-    alert('Not a bad choice!,Guitars are still better :P');
+  }
+  fullName();
+  
+  function favoriteInst()
+  {
+    var favoriteInstrument = prompt ('Which instrument do you like?');
+    if (favoriteInstrument === 'guitar'){
+      alert ('Congratulations  next Bob Marley');
     }
-
-}
+    else 
+    {
+      alert ('Not a bad choice!, Guitars are still better :P');
+    }
+  }
+  favoriteInst();
